5134b2eb49be192cc16d051a5b3697360af32c05
unknown <aeneasreina2@gmail.com>
2020-11-06 15:25:47 +0100

    'test2'

---
+++ GeometryGenerator/Cube.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace GeometryGenerator
+{
+    public class Cube
+    {
+        GeometryModel3D cube = new GeometryModel3D();
+        MeshGeometry3D cubeMesh = new MeshGeometry3D();
+        DiffuseMaterial cubeFMat = new DiffuseMaterial(new SolidColorBrush(Color.FromRgb(0,0,0))), cubeBMat = new DiffuseMaterial(new SolidColorBrush(Color.FromRgb(255,0,0)));
+        Int32Collection cubeInd = new Int32Collection(36);
+        Vector3DCollection cubeDisplacement = new Vector3DCollection(8);
+
+        public Cube()
+        {
+            createVector();
+            cubeMesh.Positions = cubePoints(new Point3D(0, 0, 0));
+            for(int i = 0; i < 6; i++)
+            {
+                SetIndex(i);
+            }
+            cubeMesh.TriangleIndices = cubeInd;
+            cube.Geometry = cubeMesh;
+            cube.Material = cubeFMat;
+            cube.BackMaterial = cubeBMat;
+        }
+
+        public Cube(Point3D centerP)
+        {
+            createVector();
+            cubeMesh.Positions = cubePoints(centerP);
+            for(int i = 0;i < 6;i++)
+            {
+                SetIndex(i);
+            }
+            cubeMesh.TriangleIndices = cubeInd;
+            cube.Geometry = cubeMesh;
+            cube.Material = cubeFMat;
+            cube.BackMaterial = cubeBMat;
+        }
+
+        public Cube(int scale)
+        {
+            createVector();
+            cubeMesh.Positions = cubePoints(new Point3D(0,0,0),scale);
+            for (int i = 0; i < 6; i++)
+            {
+                SetIndex(i);
+            }
+            cubeMesh.TriangleIndices = cubeInd;
+            cube.Geometry = cubeMesh;
+            cube.Material = cubeFMat;
+            cube.BackMaterial = cubeBMat;
+        }
+
+        public Cube(Point3D centerP,int scale)
+        {
+            createVector();
+            cubeMesh.Positions = cubePoints(centerP,scale);
+            for (int i = 0; i < 6; i++)
+            {
+                SetIndex(i);
+            }
+            cubeMesh.TriangleIndices = cubeInd;
+            cube.Geometry = cubeMesh;
+            cube.Material = cubeFMat;
+            cube.BackMaterial = cubeBMat;
+        }
+
+        private void createVector()
+        {
+            int z = -1, marker = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                if (i > 3)
+                {
+                    z = 1;
+                    marker = i - 4;
+                } else
+                {
+                    z = -1;
+                    marker = i;
+                }
+                switch (marker)
+                {
+                    case 0:
+                        cubeDisplacement.Add(new Vector3D(-1, 1, z));
+                        break;
+                    case 1:
+                        cubeDisplacement.Add(new Vector3D(1, 1, z));
+                        break;
+                    case 2:
+                        cubeDisplacement.Add(new Vector3D(-1, -1, z));
+                        break;
+                    case 3:
+                        cubeDisplacement.Add(new Vector3D(1, -1, z));
+                        break;
+                    default:
+                        throw new Exception("Something went wrong. The value for the int marker is out of range. Marker value:" + marker.ToString());
+                }
+            }
+        }
+
+        private Point3DCollection cubePoints(Point3D center)
+        {
+            Point3D newPoint = new Point3D();
+            Point3DCollection value = new Point3DCollection(8);
+            foreach(Vector3D vector in cubeDisplacement)
+            {
+                newPoint = center + vector;
+                value.Add(newPoint);
+            }
+            return value;
+        }
+
+        private Point3DCollection cubePoints(Point3D center, int scale)
+        {
+            Point3D newPoint = new Point3D();
+            Point3DCollection value = new Point3DCollection(8);
+            foreach (Vector3D vector in cubeDisplacement)
+            {
+                newPoint = center + (vector*scale);
+                value.Add(newPoint);
+            }
+            return value;
+        }
+
+        private void SetIndex(int i)
+        {
+            switch (i)
+            {
+                //Front
+                case 0:
+                    cubeInd.Add(0); cubeInd.Add(1); cubeInd.Add(2); cubeInd.Add(3); cubeInd.Add(2); cubeInd.Add(1);
+                    break;
+                //Back
+                case 1:
+                    cubeInd.Add(6); cubeInd.Add(5); cubeInd.Add(4); cubeInd.Add(5); cubeInd.Add(6); cubeInd.Add(7);
+                    break;
+                //Top
+                case 2:
+                    cubeInd.Add(0); cubeInd.Add(4); cubeInd.Add(5); cubeInd.Add(5); cubeInd.Add(1); cubeInd.Add(0);
+                    break;
+                //Bottom
+                case 3:
+                    cubeInd.Add(7); cubeInd.Add(6); cubeInd.Add(2); cubeInd.Add(2); cubeInd.Add(3); cubeInd.Add(7);
+                    break;
+                //Left
+                case 4:
+                    cubeInd.Add(4); cubeInd.Add(0); cubeInd.Add(2); cubeInd.Add(2); cubeInd.Add(6); cubeInd.Add(4);
+                    break;
+                //Right
+                case 5:
+                    cubeInd.Add(3); cubeInd.Add(1); cubeInd.Add(5); cubeInd.Add(5); cubeInd.Add(7); cubeInd.Add(3);
+                    break;
+                //0<Number>5 Creates Exception
+                default:
+                    throw new Exception("Wrong Number for i. i = " + i.ToString());
+            }
+        }
+
+        public GeometryModel3D ToGeometryModel3D()
+        {
+            return cube;
+        }
+
+    }
+}
---
+++ GeometryGenerator/Triangle.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace GeometryGenerator
+{
+    public class Triangle
+    {
+        private GeometryModel3D triangle = new GeometryModel3D();
+        private MeshGeometry3D preMesh = new MeshGeometry3D();
+        private Point3DCollection points = new Point3DCollection(3);
+        private Int32Collection triangleIndices = new Int32Collection(3);
+        private DiffuseMaterial materialF = new DiffuseMaterial(new SolidColorBrush(Color.FromRgb(0, 0, 0))), materialB = new DiffuseMaterial(new SolidColorBrush(Color.FromRgb(255, 0, 0)));
+
+        //Constructors still have to finish one
+        #region
+        public Triangle()
+        {
+            //Setting Material
+            triangle.Material = materialF; triangle.BackMaterial = materialB;
+            //Setting der positions of every Vertex
+            points.Add(new Point3D(0, 0, 0));
+            points.Add(new Point3D(0, 5, 0));
+            points.Add(new Point3D(-5, 0, 0));
+            //Setting the triangle Indices
+            for (int i = 0; i < 3; i++)
+            {
+                triangleIndices.Add(i);
+            }
+            //Assigning points and indices to the premesh
+            preMesh.Positions = points;
+            preMesh.TriangleIndices = triangleIndices;
+            //Assigning the premesh as the triangles geometry
+            triangle.Geometry = preMesh;
+        }
+
+        public Triangle(Point3D a, Point3D b, Point3D c)
+        {
+            
+        }
+        #endregion
+
+        //Material Properties Finished
+        #region
+        public DiffuseMaterial MaterialF
+        {
+            get { return materialF;}
+            set { materialF = value;}
+        }
+
+        public DiffuseMaterial MaterialB
+        {
+            get { return materialB;}
+            set { materialB = value;}
+        }
+        #endregion
+
+        //Functions Transform functions dont work
+        #region
+        
+
+        public GeometryModel3D ToGeometryModel3D()
+        {
+            return triangle;
+        }
+
+        public void Translate(int x, int y,int z)
+        {
+            TranslateTransform3D translateTransform3D = new TranslateTransform3D(x, y, z);
+            preMesh = (MeshGeometry3D)triangle.Geometry;
+            Point3D[] pointArr = new Point3D[preMesh.Positions.Count];
+            int counter = 0;
+            foreach(Point3D point3D in preMesh.Positions)
+            {
+                if(counter > preMesh.Positions.Count)
+                {
+                    break;
+                }
+                pointArr[counter] = point3D;
+                counter += 1;
+            }
+            points = new Point3DCollection(preMesh.Positions.Count);
+            foreach(Point3D point3D in pointArr)
+            {
+                points.Add(point3D);
+            }
+            preMesh.Positions = points;
+            triangle.Geometry = preMesh;
+        }
+
+        public void Scale(int scale)
+        {
+            ScaleTransform3D scaleTransform3D = new ScaleTransform3D(scale, scale, scale);
+            preMesh = (MeshGeometry3D)triangle.Geometry;
+            Point3D[] pointArr = new Point3D[preMesh.Positions.Count];
+            int counter = 0;
+            foreach (Point3D point3D in preMesh.Positions)
+            {
+                if (counter > preMesh.Positions.Count)
+                {
+                    break;
+                }
+                pointArr[counter] = point3D;
+                counter += 1;
+            }
+            points = new Point3DCollection(preMesh.Positions.Count);
+            foreach (Point3D point3D in pointArr)
+            {
+                points.Add(point3D);
+            }
+            preMesh.Positions = points;
+            triangle.Geometry = preMesh;
+        }
+
+        public void Scale(int x, int y, int z)
+        {
+            ScaleTransform3D scaleTransform3D = new ScaleTransform3D(x,y,z);
+            preMesh = (MeshGeometry3D)triangle.Geometry;
+            Point3D[] pointArr = new Point3D[preMesh.Positions.Count];
+            int counter = 0;
+            foreach(Point3D point3D in preMesh.Positions)
+            {
+                if(counter > preMesh.Positions.Count)
+                {
+                    break;
+                }
+                pointArr[counter] = point3D;
+                counter += 1;
+            }
+            points = new Point3DCollection(preMesh.Positions.Count);
+            foreach(Point3D point3D in pointArr)
+            {
+                points.Add(point3D);
+            }
+            preMesh.Positions = points;
+            triangle.Geometry = preMesh;
+        }
+
+        #endregion
+    }
+}
